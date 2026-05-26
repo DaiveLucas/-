637de4a28c2b7757f7bab531acc6fa879e4ac1fd
DaiveLucas <l15555960271@gmail.com>
2026-05-26 08:14:07 +0800

    ??:????????????????????

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://usmucbgltajiipvpuixp.supabase.co'
-const supabasePublishableKey = 'sb_publishable_O8tRB0CeVp4yl9KWqEvkGA_5ty80OI1'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabasePublishableKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey)
